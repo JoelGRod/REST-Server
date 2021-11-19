51e6411a3b06bd29ffe81b8dea0b7517c96bd462
joelgrod <joelgrod@gmail.com>
2021-11-19 16:32:10 +0000

    started with jest tests

---
+++ src/user/test/validators/checkId.spec.ts
@@ -0,0 +1,30 @@
+import { checkId } from "../../main/validators";
+import { UserDb } from "../../../shared/dbModels";
+
+describe("Domain User - Validators - checkId", () => {
+    // State
+    // Tests
+    test("Returns true if userDb Exists and status is true", async () => {
+        // arrange
+        const uid = "618fc61c198f16d3b1a21157";
+        UserDb.findById = jest.fn().mockReturnValueOnce({status: true});
+        // act
+        const userDb = await checkId(uid);
+        // assert
+        expect(userDb).toBe(true);
+        expect(UserDb.findById).toHaveBeenCalledWith(uid);
+    })
+    
+    test("Returns Error if userDb not Exists", async () => {
+        // arrange
+        const uid = "";
+        UserDb.findById = jest.fn().mockReturnValueOnce({});
+        // act
+        // Assert
+        await expect(checkId(uid))
+            .rejects
+            .toThrow("The provided id does not exist");
+        expect(UserDb.findById).toHaveBeenCalledWith(uid);
+    })
+});
+
